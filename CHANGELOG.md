35290e4ea0cf7cb4d11fae7a4e0447f615b60620
Velu S Gautam <i@velusgautam.com>
2021-04-26 15:45:31 +0200

    new changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,8 @@
 # Hello world javascript action
 
-This action prints "Hello World" or "Hello" + the name of a person to greet to the log.
-FR-14330 New icon sandbox page
-FR-13953 Feature toggle info card terminal rights
-FR-14207 Unable to update active field for CapabilityDefinitions
-FR-10404 confirm dialog and information dialog design changes batch 1
-FR-13815 Update dropzones
-FR-12691 Allow multiple refunds for VAT
+- FR-14330 New icon sandbox page
+- FR-13953 Feature toggle info card terminal rights
+- FR-14207 Unable to update active field for CapabilityDefinitions
+- FR-10404 confirm dialog and information dialog design changes batch 1
+- FR-13815 Update dropzones
+- FR-12691 Allow multiple refunds for VAT
--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 // const exec = require('@actions/exec');
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  console.log('process.cwd()');
-  console.log(process.cwd());
-  console.log('---end---');
-  console.log(__dirname);
-  console.log('---end---');
-  const directoryPath = __dirname;
+  // const nameToGreet = core.getInput('who-to-greet');
+  // console.log(`Hello ${nameToGreet}!`);
+  // console.log('process.cwd()');
+  // console.log(process.cwd());
+  // console.log('---end---');
+  // console.log(__dirname);
+  // console.log('---end---');
+  // const directoryPath = __dirname;
 
-  fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-      return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    console.log('---directoryPath ---');
-    files.forEach(function (file) {
-      // Do whatever you want to do with the file
-      console.log(file);
-    });
-    console.log('---directoryPath ---');
-  });
-
+  // fs.readdir(directoryPath, function (err, files) {
+  //   //handling error
+  //   if (err) {
+  //     return console.log('Unable to scan directory: ' + err);
+  //   }
+  //   //listing all files using forEach
+  //   console.log('---directoryPath ---');
+  //   files.forEach(function (file) {
+  //     // Do whatever you want to do with the file
+  //     console.log(file);
+  //   });
+  //   console.log('---directoryPath ---');
+  // });
+  console.log(github.context.payload.pull_request.body);
   const payload = JSON.stringify(
     github.context.payload.pull_request.body,
     undefined,
@@ -36,18 +36,18 @@ try {
   );
   console.log(`The event payload is: ${payload}`);
 
-  const changelogPath = path.join(__dirname, 'CHANGELOG.md');
-  console.log('---changelogPath ---');
-  console.log(changelogPath);
-  const data = fs.readFileSync(changelogPath, 'utf8');
-  const newData = data + payload;
+  // const changelogPath = path.join(__dirname, 'CHANGELOG.md');
+  // console.log('---changelogPath ---');
+  // console.log(changelogPath);
+  // const data = fs.readFileSync(changelogPath, 'utf8');
+  // const newData = data + payload;
 
-  console.log(`New Changelog is : ${newData}`);
+  // console.log(`New Changelog is : ${newData}`);
 
   // fs.writeFileSync(changelogPath, newData);
   // // Log this message if the file was written to successfully
   // console.log('wrote to file successfully');
-  const finalData = newData.replace(/"/g, "'").toString();
+  const finalData = payload.replace(/"/g, "'").toString();
   // const time = new Date().toTimeString();
   core.setOutput('time', finalData);
   // Get the JSON webhook payload for the event that triggered the workflow
