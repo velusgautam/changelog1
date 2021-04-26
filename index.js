69b8bcdbad639a2b319bb210802dfd5b905fc99e
Velu S Gautam <i@velusgautam.com>
2021-04-26 12:43:15 +0200

    another test

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ try {
   console.log(__dirname);
   console.log('---end---');
   const directoryPath = __dirname;
-  console.log('---directoryPath ---');
+
   fs.readdir(directoryPath, function (err, files) {
     //handling error
     if (err) {
@@ -28,51 +28,45 @@ try {
     });
     console.log('---directoryPath ---');
   });
-  console.log('---directoryPath end---');
-  fs.readdir(process.cwd(), function (err, files) {
-    //handling error
-    if (err) {
-      return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    console.log('---process start---');
-    files.forEach(function (file) {
-      // Do whatever you want to do with the file
-      console.log(file);
-    });
-    console.log('---process end end---');
-  });
-  console.log('---process end end---');
-  const time = new Date().toTimeString();
-  core.setOutput('time', time);
-  // Get the JSON webhook payload for the event that triggered the workflow
+
   const payload = JSON.stringify(
     github.context.payload.pull_request.body,
     undefined,
     2
   );
   console.log(`The event payload is: ${payload}`);
+
+  const changelogPath = path.join(__dirname, 'CHANGELOG.md');
+  console.log('---changelogPath ---');
+  console.log(changelogPath);
+  fs.readFile(changelogPath, (err, data) => {
+    // if there's an error, log it and return
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    const newData = data.toString + payload;
+
+    console.log(`New Changelog is : ${newData}`);
+
+    // fs.writeFile('./CHANGELOG.md', newData, (err) => {
+    //   // If there is any error in writing to the file, return
+    //   if (err) {
+    //     console.error(err);
+    //     return;
+    //   }
+
+    //   // Log this message if the file was written to successfully
+    //   console.log('wrote to file successfully');
+    // });
+  });
+
+  const time = new Date().toTimeString();
+  core.setOutput('time', time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+
   // await exec.exec('node', ['changelog.js', 'foo=bar']);
-  // fs.readFile('./CHANGELOG.md', (err, data) => {
-  //   // if there's an error, log it and return
-  //   if (err) {
-  //     console.error(err);
-  //     return;
-  //   }
-
-  //   const newData = data.toString + payload;
-
-  //   fs.writeFile('./CHANGELOG.md', newData, (err) => {
-  //     // If there is any error in writing to the file, return
-  //     if (err) {
-  //       console.error(err);
-  //       return;
-  //     }
-
-  //     // Log this message if the file was written to successfully
-  //     console.log('wrote to file successfully');
-  //   });
-  // });
 } catch (error) {
   core.setFailed(error.message);
 }
