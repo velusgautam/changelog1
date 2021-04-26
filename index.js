87e45247bc8484533d45b1694daf790252f15a8e
Velu S Gautam <i@velusgautam.com>
2021-04-26 12:02:31 +0200

    TEST

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -14,6 +15,26 @@ try {
     2
   );
   console.log(`The event payload is: ${payload}`);
+  fs.readFile('CHANGELOG.md', (err, data) => {
+    // if there's an error, log it and return
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    const newData = data.toString + payload;
+
+    fs.writeFile('CHANGELOG.md', newData, (err) => {
+      // If there is any error in writing to the file, return
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      // Log this message if the file was written to successfully
+      console.log('wrote to file successfully');
+    });
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
